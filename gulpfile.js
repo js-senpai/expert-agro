de13441fed9cf64bb43272901bf847d92462c0e9
ka <andrewkarapenkov@gmail.com>
2020-05-08 22:01:34 +0300

    add new fixes

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('styles', function() {
 	.pipe(concat('styles.min.css'))
 	.pipe(autoprefixer({
 		grid: true,
-		overrideBrowserslist: ['last 10 versions']
+		overrideBrowserslist: ['>1%']
 	}))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
 	.pipe(gulp.dest('app/css'))
